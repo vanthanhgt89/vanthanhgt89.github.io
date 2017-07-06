3e6d836e5d9aa3c67e0808e3b884705b09d32fc3
vanthanhgt89 <vanthanhgt89@gmail.com>
2017-07-06 16:27:21 +0700

    up

--- Kaban Board/js/index.js
+++ Kaban Board/js/index.js
@@ -1,10 +1,25 @@
 var app = {
 	newJob: function (e, type, input){
-		var newjpb = $('input').val()
+		var jobName = $(input).val();
 		var event = window.event || e;
-		if (event.keycode == 13 && ){
-			
+		if (event.keyCode === 13 && jobName.trim() !== ''){
+			this.addJobToList(type,jobName);
+
+			$(input).val('');
 		}
+	},
+	addJobToList: function(type, jobName) {
+		var html = '<li class="collection-item ui-state-default"> '+ jobName +'<span class="badge" onclick="app.deleteJob(this)"><i class="material-icons">delete</i></span> </li>';
+		$('#' + type ).append(html);
+	},
+	deleteJob: function(span){
+		var modal = $('#modal-confirm');
+		var item = $(span).parent();
+		modal.modal('open');
+		$('#btn-delete').on('click', function (){
+			item.remove();
+			modal.modal('close');
+		})
 	}
 }
 
